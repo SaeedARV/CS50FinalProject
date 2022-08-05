7a1a3d8c78f5362c365cb8a7b8753a6a6e8156e1
SaeedARV <mohammadsaeedarvenaghi@gmail.com>
2022-08-05 12:10:24 +0430

    added all 0 button, a*b button, and result section

--- js/script.js
+++ js/script.js
@@ -1,27 +1,30 @@
 var arow = 3, brow = 3, acolumn = 3, bcolumn = 3;
-var A = new Array(10), B = new Array(10);
+var A = new Array(10), B = new Array(10), result = new Array(10);
 for (var i = 0; i < 10; i++) {
-  A[i] = new Array(10);
-  B[i] = new Array(10);
-  for (var j = 0; j < 10; j++){
-    A[i][j] = '0';
-    B[i][j] = '0';
-  }
+    A[i] = new Array(10);
+    B[i] = new Array(10);
+    result[i] = new Array(10);
+    for (var j = 0; j < 10; j++) {
+        A[i][j] = 0;
+        B[i][j] = 0;
+        result[i][j] = 0;
+    }
 }
 
 function makeTable(str, row, column) {
-    var clear = document.getElementById(str);
-    clear.innerHTML = "";
-
+    document.getElementById(str).innerHTML = "";
     for (var r = 0; r < row; r++) {
         var x = document.getElementById(str).insertRow(r);
         for (var c = 0; c < column; c++) {
             var y = x.insertCell(c);
-            if(str === "atable"){
-                y.innerHTML = '<input type="text" value=' + A[r][c]  + ' class="fourChar">';
+            if (str === "atable") {
+                y.innerHTML = '<input type="text" value=' + A[r][c] + ' class="fourChar">';
             }
-            else{
-                y.innerHTML = '<input type="text" value=' + B[r][c]  + ' class="fourChar">';
+            else if (str === "btable") {
+                y.innerHTML = '<input type="text" value=' + B[r][c] + ' class="fourChar">';
+            }
+            else {
+                y.innerHTML = '<input type="text" value=' + result[r][c] + ' class="fourChar">';
             }
         }
     }
@@ -46,21 +49,68 @@ function updateRowColumn(str) {
     }
 }
 
-function fillRandom(str){ 
-    if(str === "atable"){
+function fillRandom(str) {
+    if (str === "atable") {
         for (var r = 0; r < 10; r++) {
             for (var c = 0; c < 10; c++) {
-                A[r][c] = eval(Math.floor(Math.random() * 11));
+                A[r][c] = Math.floor(Math.random() * 11);
             }
         }
-        makeTable('atable', arow, acolumn)
+        makeTable('atable', arow, acolumn);
     }
-    else{
+    else {
         for (var r = 0; r < 10; r++) {
             for (var c = 0; c < 10; c++) {
-                B[r][c] = eval(Math.floor(Math.random() * 11));
+                B[r][c] = Math.floor(Math.random() * 11);
             }
         }
-        makeTable('btable', brow, bcolumn)
+        makeTable('btable', brow, bcolumn);
     }
+}
+
+function clearTable(str) {
+    if (str === "atable") {
+        for (var r = 0; r < 10; r++) {
+            for (var c = 0; c < 10; c++) {
+                A[r][c] = 0;
+            }
+        }
+        makeTable('atable', arow, acolumn);
+    }
+    else {
+        for (var r = 0; r < 10; r++) {
+            for (var c = 0; c < 10; c++) {
+                B[r][c] = 0;
+            }
+        }
+        makeTable('btable', brow, bcolumn);
+    }
+}
+
+function invalidOp(code){
+    document.getElementById('rtable').innerHTML = "";
+    if(code === 0){
+        document.getElementById('invalidOp').innerText = "The number of columns in the matrix A must be equal to the number of rows in the matrix B."
+    }
+}
+
+function multiplication(){
+
+    if(acolumn != brow){
+        invalidOp(0);
+        return;
+    }
+
+    document.getElementById('invalidOp').innerHTML = "";
+
+    for(var i = 0; i < arow; i++){
+        for(var j = 0; j < bcolumn; j++){
+            result[i][j] = 0;
+            for(var t = 0; t < brow; t++){
+                result[i][j] += (A[i][t])*(B[t][j]);
+                console.log(result[0][0]);
+            }
+        }
+    }
+    makeTable('rtable', arow, bcolumn);
 }
