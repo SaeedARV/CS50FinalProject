d722b0d1e8578178de12ab06483b2a7100f6c4d6
SaeedARV <mohammadsaeedarvenaghi@gmail.com>
2022-08-05 16:05:20 +0430

    fixed changing cells

--- js/script.js
+++ js/script.js
@@ -13,18 +13,18 @@ for (var i = 0; i < 10; i++) {
 
 function makeTable(str, row, column) {
     document.getElementById(str).innerHTML = "";
-    for (var r = 0; r < row; r++) {
-        var x = document.getElementById(str).insertRow(r);
-        for (var c = 0; c < column; c++) {
-            var y = x.insertCell(c);
+    for (var i = 0; i < row; i++) {
+        var x = document.getElementById(str).insertRow(i);
+        for (var j = 0; j < column; j++) {
+            var y = x.insertCell(j);
             if (str === "atable") {
-                y.innerHTML = '<input type="text" value=' + A[r][c] + ' class="fourChar">';
+                y.innerHTML = '<input type="text" value=' + A[i][j] + ' class="largeCell" onblur="updateCells(' + "'atable', " + i + ', ' + j + ' )">';
             }
             else if (str === "btable") {
-                y.innerHTML = '<input type="text" value=' + B[r][c] + ' class="fourChar">';
+                y.innerHTML = '<input type="text" value=' + B[i][j] + ' class="largeCell" onblur="updateCells(' + "'btable', " + i + ', ' + j + ' )">';
             }
             else {
-                y.innerHTML = '<input type="text" value=' + result[r][c] + ' class="fourChar">';
+                y.innerHTML = '<input type="text" value=' + result[i][j] + ' class="largeCell" readonly="readonly">';
             }
         }
     }
@@ -49,19 +49,28 @@ function updateRowColumn(str) {
     }
 }
 
+function updateCells(str, i, j) {
+    if(str === "atable"){
+        A[i][j] = eval(document.getElementById('atable').rows[i].cells[j].children[0].value);
+    }
+    else {
+        B[i][j] = eval(document.getElementById('btable').rows[i].cells[j].children[0].value);
+    }
+}
+
 function fillRandom(str) {
     if (str === "atable") {
-        for (var r = 0; r < 10; r++) {
-            for (var c = 0; c < 10; c++) {
-                A[r][c] = Math.floor(Math.random() * 11);
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                A[i][j] = Math.floor(Math.random() * 11);
             }
         }
         makeTable('atable', arow, acolumn);
     }
     else {
-        for (var r = 0; r < 10; r++) {
-            for (var c = 0; c < 10; c++) {
-                B[r][c] = Math.floor(Math.random() * 11);
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                B[i][j] = Math.floor(Math.random() * 11);
             }
         }
         makeTable('btable', brow, bcolumn);
@@ -70,46 +79,67 @@ function fillRandom(str) {
 
 function clearTable(str) {
     if (str === "atable") {
-        for (var r = 0; r < 10; r++) {
-            for (var c = 0; c < 10; c++) {
-                A[r][c] = 0;
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                A[i][j] = 0;
             }
         }
         makeTable('atable', arow, acolumn);
     }
     else {
-        for (var r = 0; r < 10; r++) {
-            for (var c = 0; c < 10; c++) {
-                B[r][c] = 0;
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                B[i][j] = 0;
             }
         }
         makeTable('btable', brow, bcolumn);
     }
 }
 
-function invalidOp(code){
+function invalidOp(code) {
     document.getElementById('rtable').innerHTML = "";
-    if(code === 0){
+    document.getElementById('op').innerHTML = "";
+    if (code === 0) {
         document.getElementById('invalidOp').innerText = "The number of columns in the matrix A must be equal to the number of rows in the matrix B."
+    }
+    else if (code == 1) {
+        document.getElementById('invalidOp').innerText = "Size of matrix A must be equal to the size of matrix B."
     }
 }
 
-function multiplication(){
+function multiplication() {
 
-    if(acolumn != brow){
+    if (acolumn != brow) {
         invalidOp(0);
         return;
     }
 
     document.getElementById('invalidOp').innerHTML = "";
+    document.getElementById('op').innerHTML = "A * B =";
 
-    for(var i = 0; i < arow; i++){
-        for(var j = 0; j < bcolumn; j++){
+    for (var i = 0; i < arow; i++) {
+        for (var j = 0; j < bcolumn; j++) {
             result[i][j] = 0;
-            for(var t = 0; t < brow; t++){
-                result[i][j] += (A[i][t])*(B[t][j]);
-                console.log(result[0][0]);
+            for (var t = 0; t < brow; t++) {
+                result[i][j] += (A[i][t]) * (B[t][j]);
             }
+        }
+    }
+    makeTable('rtable', arow, bcolumn);
+}
+
+function addition() {
+    if (arow != brow || acolumn != bcolumn) {
+        invalidOp(1);
+        return;
+    }
+
+    document.getElementById('invalidOp').innerHTML = "";
+    document.getElementById('op').innerHTML = "A + B =";
+
+    for (var i = 0; i < arow; i++) {
+        for (var j = 0; j < acolumn; j++) {
+            result[i][j] = A[i][j] + B[i][j];
         }
     }
     makeTable('rtable', arow, bcolumn);
